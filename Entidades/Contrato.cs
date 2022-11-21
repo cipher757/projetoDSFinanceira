32cd28c398dbaa0c61f21b4827ea528efb24950a
vinisvp <viniciuspedro0905@gmail.com>
2022-11-21 17:21:47 -0300

    Adicionado os atributos e metodos da Classe Contrato

--- Entidades/Contrato.cs
+++ Entidades/Contrato.cs
@@ -6,5 +6,34 @@ namespace projetoFinanceira.Entidades
 {
     class Contrato
     {
+        public int Numero { get; set; }
+        public string Contratante { get; set; }
+        public double Valor { get; set; }
+        public int Prazo { get; set; }
+
+        public Contrato() { }
+
+        public Contrato(int numero, string contratante, double valor, int prazo)
+        {
+            Numero = numero;
+            Contratante = contratante;
+            Valor = valor;
+            Prazo = prazo;
+        }
+
+        public virtual double calcularPrestacao()
+        {
+            return Valor / Prazo;
+        }
+
+        public virtual string exibirInfo()
+        {
+            return "Valor do contrato: "
+            + Valor.ToString("F2", CultureInfo.InvariantCulture)
+            + "\r\nPrazo do contrato: "
+            + Prazo
+            + " meses\r\nValor da prestação: "
+            + calcularPrestacao().ToString("F2", CultureInfo.InvariantCulture);
+        }
     }
 }
