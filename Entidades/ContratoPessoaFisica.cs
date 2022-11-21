92269fdbe5e1f35c5b3ee58352188cbc9c51e8c2
vinisvp <viniciuspedro0905@gmail.com>
2022-11-21 18:28:42 -0300

    Modificações na Classe Pessoa Fisica

--- Entidades/ContratoPessoaFisica.cs
+++ Entidades/ContratoPessoaFisica.cs
@@ -6,6 +6,21 @@ namespace projetoFinanceira.Entidades
 {
     class ContratoPessoaFisica : Contrato
     {
+        public int Cpf { get; set; }
+        public DateTime DataNasc { get; set; }
+        public DateTime DataAtual { get; set; }
 
+        public ContratoPessoaFisica() { }
+
+        public ContratoPessoaFisica(int numero, string contratante, double valor, int prazo, int cpf, DateTime dataNasc, DateTime dataAtual)
+        {
+
+        }
+
+        public override double calcularPrestacao()
+        {
+            
+            if ()
+        }
     }
 }
